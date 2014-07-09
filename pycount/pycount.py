77ff9fee6195fbf683efd39a227fcbd7594744e7
Tihomir Saulic <Tihomir.Saulic@wotifgroup.com>
2014-07-09 10:22:47 +1000

    Alpha version final.

--- pycount/pycount.py
+++ pycount/pycount.py
@@ -9,7 +9,7 @@
    Core pyCount file
 
    :copyright: (c) Tihomir Saulic
-   :license: BSD, see LICENSE for more details
+   :license: GPL 2.0, see LICENSE for more details
 """
 
 
@@ -17,6 +17,12 @@ from __future__ import print_function
 
 import hashlib
 import os
+import sys
+
+try:
+    from binaryornot.check import is_binary
+except ImportError as an_error:
+    sys.exit(an_error)
 
 
 class Counter(object):
@@ -46,24 +52,60 @@ class Counter(object):
                 return
             yield chunk
 
-    def unique(self, filepath, hash=hashlib.sha1, unique=False):
-        hashobj = hash()
-        for chunk in self.chunk_reader(open(filepath, 'rb')):
-                    hashobj.update(chunk)
-        file_id = (hashobj.digest(), os.path.getsize(filepath))
+    def unique(self, a_file, hashing=hashlib.sha1, unique=False):
+        """Filters out duplicate files
+        """
+        hashobj = hashing()
+        for chunk in self.chunk_reader(open(a_file, "rb")):
+            hashobj.update(chunk)
+        file_id = (hashobj.digest(), os.path.getsize(a_file))
         duplicate = self.hashes.get(file_id, None)
         if duplicate:
-            print("Duplicate found: %s and %s" % (filepath, duplicate))
+            #print("Duplicate found: %s and %s" % (a_file, duplicate))
+            pass
         else:
-            self.hashes[file_id] = filepath
+            self.hashes[file_id] = a_file
             unique = True
         return unique
 
     def discover(self):
+        """Discovers all files and performs basic filters to include only
+           valid files in the list
+        """
         self.files = []
         self.hashes = {}
         for path, subpath, files in os.walk(self.root):
-            for a_file in files:
-                filepath = os.path.join(path, a_file)
-                if os.stat(filepath).st_size > 0 and self.unique(filepath):
-                    self.files.append(filepath)
+            if '/.' not in path:
+                for a_file in files:
+                    if not a_file.startswith('.'):
+                        a_file = os.path.join(path, a_file)
+                        non_empty = os.stat(a_file).st_size > 0
+                        if non_empty and self.unique(a_file) and not \
+                                is_binary(a_file):
+                            self.files.append(a_file)
+
+    def count(self):
+        """Counts lines of code for valid files in self.patterns
+           Generates and prints a decent looking breakdown report for lines
+           of code for all existent languages under our path
+        """
+        results = {}
+        for path in self.files:
+            ext = os.path.splitext(path)[1]
+            count = 0
+            if ext in self.patterns.keys():
+                with open(path, "r") as a_file:
+                    for line in a_file:
+                        if line.strip():
+                            count += 1
+                try:
+                    results[self.patterns[ext]] = results[self.patterns[ext]] \
+                        + count
+                except KeyError:
+                    results[self.patterns[ext]] = 0
+        print('Language          LOC')
+        print('-' * 21)
+        for key, value in results.items():
+            print('{0:9}     {1:7d}'.format(key, value))
+        print('-' * 21)
+        print('{0:1} {1:17d}'.format('SUM', sum(results.values())))
