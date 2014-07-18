3c0e55d43a0d60b2517af436a0b73e42e3bf6874
Tihomir Saulic <Tihomir.Saulic@wotifgroup.com>
2014-07-18 12:20:26 +1000

    Version bump because PyPi was complaining about old version format.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/tsaulic',
     'download_url': 'https://github.com/tsaulic',
     'author_email': 'tihomir[DOT]saulic[AT]gmail[DOT]com',
-    'version': '0.36a',
+    'version': '0.37.1',
     'install_requires': ['nose', 'binaryornot'],
     'packages': ['pycount'],
     'scripts': ['bin/pycount'],
