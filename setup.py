7c3cf675b0b32e2f6891624de6febe8e3a777acc
Tihomir Saulic <Tihomir.Saulic@wotifgroup.com>
2014-11-07 14:08:00 +1000

    Revert using .whl due to a bug I can't resolve yet

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'http://github.com/tsaulic/pycount',
     'download_url': 'http://github.com/tsaulic/pycount',
     'author_email': 'tihomir[DOT]saulic[AT]gmail[DOT]com',
-    'version': '0.4.8',
+    'version': '0.4.9',
     'install_requires': ['binaryornot'],
     'packages': ['pycount'],
     'scripts': ['bin/pycount'],
