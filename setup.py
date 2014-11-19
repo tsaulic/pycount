456a0dd3445f191842ca9b7da9383b02cbed4dbc
Tihomir Saulic <Tihomir.Saulic@wotifgroup.com>
2014-11-19 11:18:21 +1000

    Revert README file name and bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'http://github.com/tsaulic/pycount',
     'download_url': 'http://github.com/tsaulic/pycount',
     'author_email': 'tihomir[DOT]saulic[AT]gmail[DOT]com',
-    'version': '0.5.4',
+    'version': '0.5.5',
     'install_requires': ['binaryornot'],
     'packages': ['pycount'],
     'scripts': ['bin/pycount'],
