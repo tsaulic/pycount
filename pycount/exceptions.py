c7b234fcd172aac157089706bffd657b31976a7b
Tihomir Saulic <tsaulic@wotifgroup.com>
2015-01-21 14:49:26 +1000

    Add missing exceptions.py

---
+++ pycount/exceptions.py
@@ -0,0 +1,13 @@
+"""
+   pycount.exceptions
+   ~~~~~~~~~~~~~~~
+
+   pycount's custom exceptions
+
+   :copyright: (c) Tihomir Saulic
+   :license: DO WHAT YOU WANT TO PUBLIC LICENSE, see LICENSE for more details
+"""
+
+
+class InvalidIgnoreTypeError(Exception):
+    pass
