431d7836259a6e4ea07d08a3fa030fa07998f4c9
Tihomir Saulic <tsaulic@expedia.com>
2016-12-14 15:44:42 +1000

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'url': 'http://github.com/tsaulic/pycount',
     'download_url': 'http://github.com/tsaulic/pycount',
     'author_email': 'tihomir[DOT]saulic[AT]gmail[DOT]com',
-    'version': '0.6.9',
+    'version': '0.6.10',
     'install_requires': ['binaryornot', 'matplotlib'],
     'packages': ['pycount'],
     'scripts': ['bin/pycount'],
