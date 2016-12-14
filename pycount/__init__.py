303732aa8c6c00dfb3f91eeb9cc2da6146e2dcf1
Tihomir Saulic <tsaulic@expedia.com>
2016-12-14 15:44:15 +1000

    Remove Python 3 warning.

--- pycount/__init__.py
+++ pycount/__init__.py
@@ -9,8 +9,5 @@ def check_requirements():
     version = getattr(sys, "version_info", (0,))
     if version < (2, 6):
         raise ImportError("pycount requires python 2.6 or later.")
-    if version >= (3, 0):
-        print("WARNING: pycount might not work well with python 3 "
-              "as it was not tested.")
 
 check_requirements()
